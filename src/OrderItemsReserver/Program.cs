33cdc2f4ebfcbba7c565a06d72b70b7bb2fc1c65
DESKTOP-U4CBLI4\ketin <ketinngz@gmail.com>
2026-02-11 21:16:20 +0200

    switch to pascal case

--- src/OrderItemsReserver/Program.cs
+++ src/OrderItemsReserver/Program.cs
@@ -1,9 +1,18 @@
+using System.Text.Json;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureFunctionsWorkerDefaults(builder =>
+    {
+        // Configure JSON serialization to accept camelCase from frontend
+        builder.Services.Configure<JsonSerializerOptions>(options =>
+        {
+            options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            options.PropertyNameCaseInsensitive = true;
+        });
+    })
     .ConfigureServices(services =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
